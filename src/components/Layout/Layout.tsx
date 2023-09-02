73169c7afd26d82aadf6fa92bdcf74a5fc9a791c
Carles <cpcastells@gmail.com>
2023-09-02 18:04:10 +0200

    Create NavBar component

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "../Header/Header";
+import Navbar from "../NavBar/NavBar";
 
 const Layout = (): React.ReactElement => {
   return (
     <div className="flex flex-col items-center max-w-screen m-auto">
       <Header />
       <Outlet />
+      <Navbar />
     </div>
   );
 };
